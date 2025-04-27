993f557b88a241c782202075bfd2441f665af0a5
Jacob <rozkovakov3@mail.ru>
2025-04-27 16:43:16 +0300

    Добавлена лабораторная работа 2

--- CompanyEmployees/Controllers/WeatherForecastController.cs
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -3,27 +3,45 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyEmployees.Controllers
 {
+    //[Route("[controller]")]
+    //[ApiController]
+    //public class WeatherForecastController : ControllerBase
+    //{
+    //    private ILoggerManager _logger;
+    //    public WeatherForecastController(ILoggerManager logger)
+    //    {
+
+    //        _logger = logger;
+    //    }
+    //    [HttpGet]
+    //    public IEnumerable<string> Get()
+    //    {
+    //        _logger.LogInfo("¬от информационное сообщение от нашего контроллера значений.");
+
+    //        _logger.LogDebug("¬от отладочное сообщение от нашего контроллера значений.");
+
+    //        _logger.LogWarn("¬от сообщение предупреждени€ от нашего контроллера значений.");
+
+    //        _logger.LogError("¬от сообщение об ошибке от нашего контроллера значений.");
+    //        return new string[] { "value1", "value2" };
+    //    }
     [Route("[controller]")]
     [ApiController]
     public class WeatherForecastController : ControllerBase
     {
-        private ILoggerManager _logger;
-        public WeatherForecastController(ILoggerManager logger)
+        private readonly IRepositoryManager _repository;
+        public WeatherForecastController(IRepositoryManager repository)
         {
-            
-        _logger = logger;
+            _repository = repository;
         }
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<IEnumerable<string>> Get()
         {
-            _logger.LogInfo("¬от информационное сообщение от нашего контроллера значений.");
-           
-            _logger.LogDebug("¬от отладочное сообщение от нашего контроллера значений.");
-           
-            _logger.LogWarn("¬от сообщение предупреждени€ от нашего контроллера значений.");
-           
-            _logger.LogError("¬от сообщение об ошибке от нашего контроллера значений.");
+            _repository.Company.AnyMethodFromCompanyRepository();
+            _repository.Employee.AnyMethodFromEmployeeRepository();
             return new string[] { "value1", "value2" };
         }
     }
 }
+    
+
--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using Contracts;
+using Entities;
 using LoggerService;
+using Microsoft.EntityFrameworkCore;
+using Repository;
 
 namespace CompanyEmployees.Extensions
 {
@@ -20,5 +23,7 @@ namespace CompanyEmployees.Extensions
 
             });
         public static void ConfigureLoggerService(this IServiceCollection services) => services.AddScoped<ILoggerManager, LoggerManager>();
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) => services.AddDbContext<RepositoryContext>(opts => opts.UseSqlServer(configuration.GetConnectionString("sqlConnection"), b =>b.MigrationsAssembly("CompanyEmployees")));
+        public static void ConfigureRepositoryManager(this IServiceCollection services) => services.AddScoped<IRepositoryManager, RepositoryManager>();
     }
 }
