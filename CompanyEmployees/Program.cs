43a0949da79d0aa3639e6513e41c65d809a23a61
Jacob <rozkovakov3@mail.ru>
2025-04-27 16:43:16 +0300

    Добавлена лабораторная работа 1

---
+++ CompanyEmployees/Program.cs
@@ -0,0 +1,20 @@
+using NLog;
+
+using Microsoft.AspNetCore.Hosting;
+
+namespace CompanyEmployees;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
